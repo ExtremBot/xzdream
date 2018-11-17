bbaf56d0f129a2cda9027eb2bf64e30c1c0b8e45
ExtremBot1234484 <44088452+ExtremBot1234484@users.noreply.github.com>
2018-11-17 23:53:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,39 @@ client.user.setGame(`Don't Let Me Down`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
+
+client.on('message' , message => {
+if(message.content === '-help') {
+  var EsTeKnAN = new Discord.RichEmbed()
+  .setColor('RANDOM')
+message.author.send(`
+***__وصف عن البوت__***
+**
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+》 -bc / برودكاست
+》 -link / رابط السيرفر
+》 -clear / مسح الشات
+》 -mc / قفل الروم
+》 -umc / فتح الروم
+》 -avatar / يطلع صورتك
+》 -bans / عدد الاشخاص المبندين
+》 -ct / روم كتابي لازم تكتب اسمه
+》 -cv / روم صوتي لازم تكتب اسمه
+》 -setVoice / يسوي روم يقولك كم بلفويس
+》 -move / يسحب شخص لرومك
+》 -ban / تبنيد عضو
+》 -kick / طرد عضو
+》 -mure / اعطاء العضو ميوت
+》 -unmute / فك الميوت
+》 -rooms / يعرضلك الرومات الي بلسيرفر
+》 -roles / يعرضلك رتب الي بلسيرفر
+》 -say / البوت يكرر كلام تكتبه
+》 -id / امر ايدي
+》 -new / انشاء تذكرة
+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+**
+`);
+}
+})
   
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
